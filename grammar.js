7e96997dea0029622407994d0b9e6dbd6eae93ae
Max Guppy <skylifegame@gmail.com>
2023-03-25 14:47:14 +1000

    Add function calls

--- grammar.js
+++ grammar.js
@@ -2,10 +2,11 @@ module.exports = grammar({
   name: "typst",
 
   word: $ => $.ident,
+  inline: $ => [$._statement, $._expression],
   
   rules: {
     source_file: $ => $.content_inner,
-    content_inner: $ => repeat1(choice(
+    content_inner: $ => repeat1(prec(-1, choice(
       /[^\\~\-.*_$#\]]+/,
       $.special_punct,
       "-",
@@ -14,8 +15,8 @@ module.exports = grammar({
       seq('*', $.bold_content, '*'),
       seq('_', $.em_content, '_'),
       seq('$', $.math_content, '$'),
-      seq('#', $.statement),
-    )),
+      $.code_introducer,
+    ))),
     special_punct: $ => choice(
       "~",
       "\\",
@@ -27,67 +28,106 @@ module.exports = grammar({
     em_content: $ => repeat1(/[^_]+/),
 
     math_content: $ => repeat1(choice(
-      /[^a-zA-Z"&\\$]+/,
+      /[^a-zA-Z"&\\^_$]+/,
       $.symbol_name,
+      $.math_function,
       /[a-zA-Z]/,
       $.math_special_punct,
       $.string,
     )),
     symbol_name: $ => /[a-zA-Z][a-zA-Z.]+/,
+    math_function: $ => prec(1, seq(
+      $.symbol_name,
+      token(prec(1, "(")),
+      $.math_content,
+      token(prec(1, ")")),
+    )),
     math_special_punct: $ => choice(
       "&",
       "\\",
+      "^",
+      "_",
     ),
 
-    statement: $ => choice(
-      seq("let", $.ident, "=", $.expr),
-      seq(field("set", "set"), $.ident, $.dictionary),
-      $.expr,
+    code_introducer: $ => seq(
+      '#',
+      $._statement,
+    ),
+    _statement: $ => choice(
+      $.let_statement,
+      $.set_statement,
+      $._expression,
     ),
 
-    expr: $ => choice(
+    let_statement: $ => seq(
+      field("keyword", "let"),
+      field("object", $.ident),
+      "=",
+      field("value", $._expression),
+    ),
+
+    set_statement: $ => seq(
+      field("keyword", "set"),
+      field("object", $.ident),
+      field("parameters", $.dictionary),
+    ),
+
+    _expression: $ => choice(
       $.array,
       $.dictionary,
       $.number,
       $.string,
       $.ident,
       $.content,
-      prec(1, seq($.expr, $.function_args)),
-      prec(1, seq($.expr, $.content)),
-      prec.left(1, seq($.expr, ".", $.expr)),
-      prec.left(1, seq($.expr, "+", $.expr)),
+      $.function_call,
+      $.access,
+      $.addition,
     ),
     array: $ => seq(
       "(",
-      $.expr,
-      repeat(seq(",", $.expr)),
+      $._expression,
+      repeat(seq(",", $._expression)),
       optional(","),
       ")",
     ),
     dictionary: $ => seq(
       "(",
-      $.ident,
-      ":",
-      $.expr,
-      repeat(seq(",", $.ident, ":", $.expr)),
+      $.dict_pair,
+      repeat(seq(",", $.dict_pair)),
       optional(","),
       ")",
     ),
+    dict_pair: $ => seq(
+      field("key", $.ident),
+      ":",
+      field("value", $._expression),
+    ),
+    access: $ => prec.left(1, seq($._expression, ".", $._expression)),
+    addition: $ => prec.left(1, seq($._expression, "+", $._expression)),
     content: $ => seq(
       token(prec(1, "[")),
       $.content_inner,
       token(prec(1, "]")),
     ),
-    function_args: $ => seq(
-      token(prec(1, "(")),
-      optional(seq(
-        choice($.expr, seq($.ident, ":", $.expr)),
-        repeat(seq(",", choice($.expr, seq($.ident, ":", $.expr)))),
-        optional(","),
-      )),
-      ")",
+    function_call: $ => prec(1, seq(
+      $.ident,
+      $.function_args,
+    )),
+    function_args: $ => choice(
+      seq(
+        token(prec(1, "(")),
+        optional(seq(
+          choice($._expression, $.dict_pair),
+          repeat(seq(",", choice($._expression, $.dict_pair))),
+          optional(","),
+        )),
+        ")",
+        repeat($.content)
+      ),
+      repeat1($.content),
     ),
-    ident: $ => /[a-zA-Z_][a-zA-Z0-9_.-]*/,
+
+    ident: $ => /[a-zA-Z_][a-zA-Z0-9_-]*/,
     number: $ => /[0-9]+(\.[0-9]+)?(%|pt|mm|cm|in|em|fr|deg|rad)?/,
     string: $ => seq('"', /[^"]+/, '"'),
   }
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,5 +1,10 @@
-"let" @keyword.directive
-(statement set) @keyword.directive
+(code_introducer "#" @keyword.directive (let_statement))
+(code_introducer "#" @keyword.directive (set_statement))
+(code_introducer "#" @function (function_call))
+(let_statement keyword: "let" @keyword.directive)
+(set_statement keyword: "set" @keyword.directive object: (ident) @function)
+(function_call (ident) @function)
+(math_function (symbol_name) @function)
 (symbol_name) @variable.builtin
 (special_punct) @punctuation.special
 (math_special_punct) @punctuation.special
